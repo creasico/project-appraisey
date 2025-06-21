972be97558016405fbc5cf21e0444c3d6c8ef27d
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-06-21 20:34:11 +0700

    test: init basic tests for all resources
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ tests/Feature/Pages/DashboardTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Filament\Pages\Dashboard;
+use App\Models\User;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Livewire\livewire;
+
+it('can render the page', function () {
+    actingAs(User::factory()->asSuperAdmin()->createOne());
+
+    livewire(Dashboard::class)->assertOk();
+});
